c19c4f3ded3b00892c2042b8126a6a8e2a054342
Edanurk00 <edanurk@icloud.com>
2025-03-20 10:40:06 +0100

    test

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({extended: true}));
 
 // Simple route
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index" ,{msg: "hi"});
 });
 
 //POST til bruger log in
